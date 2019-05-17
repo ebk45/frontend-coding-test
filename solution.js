0b86b738b3a9e26c2f802d5930cf375e1bb5b485
ellie turnock <ebturnock@gmail.com>
2019-05-17 14:12:34 +0100

    :white_check_mark: add function takes x amount of arguments and totals them

---
+++ solution.js
@@ -0,0 +1,19 @@
+export function add() {
+  var total = 0
+  for (var i=0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+};
+
+export function listToObject(arr) {
+
+};
+
+export function deserialize() {
+
+};
+
+export function objectToList() {
+
+};
